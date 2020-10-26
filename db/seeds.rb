99cbbb89fcc727c4008476cdca17909f79c5a004
Griefcat <anardolilli@gmail.com>
2020-10-26 14:31:15 -0400

    breakout room closed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 #create 20 restaurants
 
 20.times do
-    restaurant.create(name: Faker::Name.name)
+    restaurant = Restaurant.create(name: Faker::Restaurant.name)
+        10.times do Dish.create(name: Faker::Food.dish)
+    end 
 end
